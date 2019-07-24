b0f09a04a21ab5f7597774d0744048191316efdf
Nikita Temnikov <cpext19@dev.vtb>
2019-07-24 18:34:25 +0300

    Empty Test project

---
+++ TestsReactiveCore/TestsReactiveCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestsReactiveCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
